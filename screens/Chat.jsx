0cf4c8448ecfd0fecc3340a8eaaee5d30e4ea965
Dmytro Kotykhin <kotykhin_d@ukr.net>
2023-12-21 13:43:11 +0100

    chat update

--- screens/Chat.jsx
+++ screens/Chat.jsx
@@ -1,27 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 export const Chat = () => {
     const [messages, setMessages] = useState([]);
+    const socket = useRef();
 
     useEffect(() => {
-        setMessages([
-            {
-                _id: 1,
-                text: 'Hello!',
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/934.jpg",
-                },
-            },
-        ]);
+        socket.current = new WebSocket(process.env.WS_SERVER);
+
+        socket.current.onopen = () => {
+            console.log('Socket open');
+        };
+        socket.current.onmessage = (event) => {
+            // console.log(event.data);
+            setMessages((prevMessages) =>
+                GiftedChat.append(prevMessages, [
+                    {
+                        _id: new Date(),
+                        text: event.data,
+                        createdAt: new Date(),
+                        user: {
+                            _id: 2,
+                            name: 'AI Therapist',
+                            avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/934.jpg',
+                        },
+                    },
+                ])
+            );
+        };
+        socket.current.onclose = () => {
+            console.log('Socket closed');
+        };
+        socket.current.onerror = (err) => {
+            console.log('Socket error', err);
+        };
     }, []);
 
     const onSend = (newMessages = []) => {
         setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
         // console.log(newMessages);
+        socket.current.send(newMessages[0].text);
     };
 
     return (
@@ -31,7 +49,9 @@ export const Chat = () => {
             user={{
                 _id: 1,
             }}
+            messagesContainerStyle={{
+                backgroundColor: '#fff',
+            }}
         />
     );
 };
-
